9f22887fe7ababfee5d3268c391f8efeab667ae1
Peyton Capers <peyton.capers27@gmail.com>
2021-11-25 21:17:35 -0800

    Created a canvas element that can be drawn on on the main landing page. Still needs more work for accuracy of mouse clicks. Linked script.js for functionality. Created a link that will route to a registration form. All forms still need to be created.

---
+++ blog_app/static/js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const journal = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    let coord = { x: 200, y: 200 };
+
+    document.addEventListener("mousedown", start);
+    document.addEventListener("mouseup", stop);
+
+    function start(event) {
+        document.addEventListener("mousemove", draw);
+        reposition(event);
+    }
+    function reposition(event) {
+        coord.x = event.clientX - canvas.offsetLeft;
+        coord.y = event.clientY - canvas.offsetTop;
+    }
+    function stop() {
+        document.removeEventListener("mousemove", draw);
+    }
+    function draw(event) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = '#000';
+        ctx.moveTo(coord.x, coord.y);
+        reposition(event);
+        ctx.lineTo(coord.x, coord.y);
+        ctx.stroke()
+    }
+})
