1bd8ca3fabd1808578c891cea7602293d8c77b68
Peyton Capers <peyton.capers27@gmail.com>
2022-01-08 11:53:20 -0800

    Created a User model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
