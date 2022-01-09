df1bb49097b1f77ebca227613c726531876d9152
Peyton Capers <peyton.capers27@gmail.com>
2022-01-08 17:26:05 -0800

    Started styling on the registration form page, added background color, styled main registration div, fixed font sizes and orientation of form elements.

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.landing),  # localhost:8000
+    path('', views.landing),  # localhost:8000
     path(
-        "registration_form", views.registration_form
+        'registration_form/', views.registration_form
     ),  # localhost:8000/registration_form
+    
 ]
