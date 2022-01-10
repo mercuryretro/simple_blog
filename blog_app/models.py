9fb1b94607f2379e36764fd9e145f0dc8c0703ea
Peyton Capers <peyton.capers27@gmail.com>
2022-01-09 21:10:51 -0800

    Created UserManager class for validations. Wrote registration validationsfor all post data fields on form. Created view for registration function to create a new User.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,6 +1,39 @@
 from django.db import models
+from time import gmtime, strftime
+from datetime import *
+import re
+import bcrypt
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 # Create your models here.
+class UserManager(models.Manager):
+    def registration_validator(self, postData):
+        errors = {}
+        #email validity
+        email = User.objects.filter(email=postData['email'])
+        if email:
+            errors['unique_email'] = "Email already in use."
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['validity'] = "Invalid email address format."
+        #first and last name validity
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = "First name should contain at least 2 characters."
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = "Last name should contain at least 2 characters."
+        #username validity
+        username = User.objects.filter(username=postData['username'])
+        if username:
+            errors['unique_username'] = "Username has already been taken."
+        if len(postData['username']) < 5:
+            errors['username_len'] = "Username must be at least 5 characters long."
+        #password validity
+        if postData['password'] != postData['confirmed_password']:
+            errors['password_match'] = "Passwords do not match, try again."
+        if len(postData['password']) < 8 and len(postData['confirmed_password']) < 8:
+            errors['password_length'] = "Password must be at least 8 characters."
+        return errors
+
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import User
+from .models import User, UserManager
+from django.contrib import messages
+import bcrypt
+import re
 
 
 # Create your views here.
@@ -11,3 +14,20 @@ def landing(request):
 
 def registration_form(request):
     return render(request, "registration_form.html")
+def register(request): #function for act of registration
+    if request.method=="GET":
+        return redirect('/')
+    else:
+        password = request.POST['password']
+        confirmed_password = request.POST['confirmed_password']
+
+        hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        # confirmed_hash = bcrypt.hashpw(
+        #     confirmed_password.encode(), bcrypt.gensalt()).decode()
+
+        new_user = User.objects.create(
+            first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], password=hashed_pw)
+        request.session['user_id'] = new_user.id
+
+        
+        return redirect('/')# actually want to redirect to a new success page for login
