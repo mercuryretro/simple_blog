427056f096df29435fae8a55e838a422ca36a0cb
Peyton Capers <peyton.capers27@gmail.com>
2022-01-23 16:15:31 -0800

    Added a Post model that has a OneToMany relationship with the User

--- blog_app/models.py
+++ blog_app/models.py
@@ -42,3 +42,10 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    text_content = models.CharField(max_length=255)
+    added_by = models.ForeignKey(User, related_name= "posts", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
